1570235979f3c2d0325e7faadecc291c7712863f
yusifquluzada <yquluzade98@gmail.com>
2023-06-26 04:21:22 +0400

    added validation to Reminders Method and To properties

--- ScheduleReminder/ScheduleReminder.Service/Dtos/ReminderDtos/ReminderPostDto.cs
+++ ScheduleReminder/ScheduleReminder.Service/Dtos/ReminderDtos/ReminderPostDto.cs
@@ -14,8 +14,8 @@ public class ReminderPostDtoValidator : AbstractValidator<ReminderPostDto>
 {
     public ReminderPostDtoValidator()
     {
-        RuleFor(x => x.To).MaximumLength(100).MinimumLength(6).NotEmpty();
+        RuleFor(x => x.To).MaximumLength(100).MinimumLength(6).EmailAddress().WithMessage("Must be valid email address!").NotEmpty();
         RuleFor(x => x.Content).MaximumLength(500).MinimumLength(1).NotEmpty();
-        RuleFor(x => x.Method).MaximumLength(8).MinimumLength(5).NotEmpty();
+        RuleFor(x => x.Method).MaximumLength(8).MinimumLength(5).Must(x=> x.ToLower().Equals("email") || x.ToLower().Equals("telegram")).WithMessage("Method must be email or telegram!").NotEmpty();
     }
 }
